adb256b42e38bead87b47ef216e45715bc24dbcd
klappy <christopher@klapp.name>
2021-12-31 10:43:05 -0500

    fixed styleguide having wrong project in name

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   usageMode: 'expand',
   exampleMode: 'expand',
-  moduleAliases: { 'simple-text-editor': path.resolve(__dirname, 'src') },
+  moduleAliases: { 'simple-text-editor-rcl': path.resolve(__dirname, 'src') },
   getComponentPathLine: componentPath => {
-    const name = path.basename(componentPath, '.jsx');
-    return `import { ${name} } from 'simple-text-editor';`;
+    const name = path.basename(componentPath, '.js');
+    return `import { ${name} } from 'simple-text-editor-rcl';`;
   },
 };
